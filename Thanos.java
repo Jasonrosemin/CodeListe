94d13812c9278baf5ff5b70d175e1d476b81530c
Jasonrosemin <53218471+Jasonrosemin@users.noreply.github.com>
2019-10-09 23:28:59 +0200

    Create Thanos.java

---
+++ Thanos.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Thanos {
+
+    public static void main(String[] args) {
+
+        // TODO 1 : Create an empty heroes list
+
+        // TODO 2 : Add those heroes to the list
+        // name: Black Widow, age: 34
+        // name: Captain America, age: 100
+        // name: Vision, age: 3
+        // name: Iron Man, age: 48
+        // name: Scarlet Witch, age: 29
+        // name: Thor, age: 1500
+        // name: Spider-Man, age: 18
+        // name: Hulk, age: 49
+        // name: Doctor Strange, age: 42
+    	 Hero hero1 = new Hero("Black Widow", 34);
+    	 Hero hero2 = new Hero("Captain America", 100);
+    	 Hero hero3 = new Hero("Vision", 3);
+    	 Hero hero4 = new Hero("Iron Man", 48);
+    	 Hero hero5 = new Hero("Scarlet Witch", 29);
+    	 Hero hero6 = new Hero("Thor", 1500);
+    	 Hero hero7 = new Hero("Spider-Man", 18);
+    	 Hero hero8 = new Hero("Hulk", 49);
+    	 Hero hero9 = new Hero("Doctor Strange", 42);
+    	 
+    	 ArrayList<Hero> liste = new ArrayList<>();
+    	 
+    	 liste.add(hero1); //index 0
+    	 liste.add(hero2); 
+    	 liste.add(hero3);
+    	 liste.add(hero4);
+    	 liste.add(hero5);
+    	 liste.add(hero6);
+    	 liste.add(hero7);
+    	 liste.add(hero8);
+    	 liste.add(hero9); //index 8
+    	 	
+    	 
+
+        // TODO 3 : It's Thor birthday, now he's 1501
+    	 hero6.setAge(1501);
+
+        // TODO 4 : Shuffle the heroes list
+    	 Collections.shuffle(liste); 
+        
+
+        // TODO 5 : Keep only the half of the list
+		ArrayList<Hero> liste2=new ArrayList<>();
+		for (int i = 0; i < liste.size()/2;i++) {
+    	    liste2.add(liste.get(i));
+    	 }
+		liste=liste2;
+
+        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+    	 for (int i = 0; i < liste.size(); i++) {
+    	 System.out.println(liste.get(i).getName());
+    	 }
+    }
+}
